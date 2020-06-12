ac7b048ee59710b8ae26602db1370ed35cb27d2e
Benjamin Gudehus <hastebrot@gmail.com>
2020-06-12 16:30:42 +0200

    (chore) Add browser project using node and esbuild.

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+(()=>{let c=Object.defineProperty,e=(b,a)=>()=>(a||(a={exports:{}},b(a.exports,a)),a.exports),f=b=>c(b,"__esModule",{value:!0}),g=(b,a)=>{f(b);for(let d in a)c(b,d,{get:a[d],enumerable:!0})};var h=e(a=>{g(a,{sayHello:()=>b});const b=()=>{console.log("hello")}});h();})();
+//# sourceMappingURL=index.js.map
